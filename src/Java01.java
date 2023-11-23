4c6225cc631e08b856df78f1044ac5bd60d9e37b
keunoh <rmsdhcjswo1@gmail.com>
2023-11-23 10:49:14 +0900

    ㅎㅎ

--- src/Java01.java
+++ src/Java01.java
@@ -1,8 +1,5 @@
 public class Java01 {
     public static void main(String[] args) {
-        System.out.println("깃허브 커밋용 1번");
-        System.out.println("fork 기능");
-        System.out.println("찜하기 기능");
-
+        System.out.println("이거 수정");
     }
 }
